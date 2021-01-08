facc7e617ad6e12e02b6c5163d13c403a91ce778
Allan5829 <allan.farca@gmail.com>
2021-01-08 14:05:30 -0800

    connects ProductsContainer to actions, small debugging edits in action and reducer files, and edits package proxy with addition of a /.

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,13 +1,9 @@
 export const getAllProducts = () => {
     return (dispatch) => {
       dispatch({ type: 'LOADING_DID_MOUNT'})
-      
-      fetch('/products')
-      .then(response => {
-          return response.json()
-        })
-      .then(p => {
-          dispatch({ type: 'INDEX_PRODUCTS', payload: p })
-        })
+        console.log("inside actions")
+      fetch('products')
+      .then(response => response.json())
+      .then(p => dispatch({ type: 'INDEX_PRODUCTS', payload: p }))
     }
   }
--- src/containers/ProductsContainer.js
+++ src/containers/ProductsContainer.js
@@ -1,13 +1,18 @@
 // container to map out each product
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { getAllProducts } from '../actions/productActions'
 
 class ProductsContainer extends Component {
+
+    componentDidMount() {
+        this.props.getAllProducts()
+    }
 
     render() {
       return (
         <div>
-          products :p
+          { this.props.loading ? "Loading..." : this.props.products }
         </div>
       )
     }
@@ -15,8 +20,9 @@ class ProductsContainer extends Component {
 
 const mapStateToProps = state => {
     return {
-        products: state.products
+        products: state.products,
+        loading: state.loading
     }
 }
 
-export default connect(mapStateToProps)(ProductsContainer);
+export default connect(mapStateToProps, { getAllProducts } )(ProductsContainer);
