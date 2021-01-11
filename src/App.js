445e9601f50373f928d69ddc20501b6058b813cf
Allan5829 <allan.farca@gmail.com>
2021-01-11 14:15:05 -0800

    saves changes to App that should've been pushed with last commit.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import './cssFolder/App.css';
 import React from 'react'
-//import { BrowserRouter as Router, Route } from 'react-router-dom';
-//import NavBar from '../components/NavBar';
 import NavContainer from './containers/NavContainer'
-
-import ProductAndFilterContainer from './containers/PAndFContainer'
-import Home from './components/HomeComponent'
 
 const App = () => (
   <div className="App">
