d2e568f402901c576677144717c9e7834d248ee3
Allan5829 <allan.farca@gmail.com>
2021-01-15 13:24:18 -0800

    edits app to render navcontainer based on internal state.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import './cssFolder/App.css';
-import React from 'react'
+import React, { Component } from 'react'
 import NavContainer from './containers/NavContainer'
+import AdminNavContainer from './containers/AdminNavContainer'
+//import { button } from 'react-router-dom';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <h2> Website </h2>
-    </header>
-    <NavContainer/>  
-  </div>
-)
+class App extends Component {
+
+  state = {
+    admin: false
+  }
+
+  switchAdmin = () => {
+    let currentState = this.state.admin
+    currentState ? currentState = false : currentState = true
+    this.setState({
+      admin: currentState
+    })
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2> Website </h2>
+        </header>
+        <button onClick={this.switchAdmin}> Admin Switch </button>
+        {this.state.admin ? <AdminNavContainer/> : <NavContainer/>}
+      </div>
+    )
+  }
+  
+}
 
 export default App
